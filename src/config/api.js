1b4ca2d5459f9f62668a7eb1c85b6948bf4a2523
AndreCorrea <andreff55@gmail.com>
2022-09-14 03:31:29 -0300

    Added temporary URL base

--- src/config/api.js
+++ src/config/api.js
@@ -1,9 +1,11 @@
 import axios from 'axios'
 
 const { REACT_APP_BASE_URL } = process.env
+const baseURL = "https://covid19.mathdro.id/api" // Temporário
 
 const api = axios.create({
-    REACT_APP_BASE_URL
+    // REACT_APP_BASE_URL
+    baseURL
 })
 
 export const showData = async () => {
